b63c4050c116da72b0b8ca5623cc99b48cda0881
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-11-28 08:43:47 +0530

    animate presence stack example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import BasicOfMotion from "./pages/BasicOfMotion.js";
 import Gesture from "./pages/Gesture.js";
+import AnimatePresenceStack from "./examples/AnimatePresenceStack.js";
 
 const App = () => {
   return (
     <div>
       {/* <BasicOfMotion /> */}
-      <Gesture />
+      {/* <Gesture /> */}
+
+      {/* Examples */}
+      <AnimatePresenceStack />
     </div>
   );
 };
