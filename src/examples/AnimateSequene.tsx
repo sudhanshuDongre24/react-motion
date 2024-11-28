97a8f79ac18baad3801caa88b7a09445270acfe1
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-11-28 09:09:42 +0530

    add animate sequecne ex

--- src/examples/AnimateSequene.tsx
+++ src/examples/AnimateSequene.tsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { useAnimate } from "motion/react";
+import React, { useEffect } from "react";
+import { useAnimate, useInView } from "motion/react";
 
 const AnimateSequene = () => {
   const [scope, animate] = useAnimate();
+  const isInView = useInView(scope);
+
+  useEffect(() => {
+    if (isInView) {
+      animate(scope.current, { opacity: 1 });
+    }
+  }, [isInView]);
 
   function sequence() {
     animate([
@@ -14,11 +21,17 @@ const AnimateSequene = () => {
   }
 
   return (
-    <div
+    /*  <div
       className="w-[150px] h-[150px] rounded-[30px] bg-white cursor-pointer"
       ref={scope}
       onClick={sequence}
-    />
+    /> */
+
+    <ul ref={scope}>
+      <li />
+      <li />
+      <li />
+    </ul>
   );
 };
 
