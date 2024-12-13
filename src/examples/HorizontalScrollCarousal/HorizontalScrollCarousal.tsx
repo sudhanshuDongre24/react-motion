fea0b6237cab4f4ed11fda2b4828d194c72b1906
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-12-13 08:48:18 +0530

    add project horizontal scroll carousal

--- src/examples/HorizontalScrollCarousal/HorizontalScrollCarousal.tsx
+++ src/examples/HorizontalScrollCarousal/HorizontalScrollCarousal.tsx
@@ -1,4 +1,4 @@
-import { useScroll, useTransform } from "motion/react";
+import { useScroll, useTransform, motion } from "motion/react";
 import React from "react";
 
 const HorizontalScrollCarousal = () => {
@@ -25,7 +25,79 @@ const HorizontScrollComponent = () => {
   const targetRef = React.useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "95%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
 
-  return <section ref={targetRef} className="relative"></section>;
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky flex top-0 h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
+
+const Card = ({ card }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      >
+        <div className="absolute inset-0 z-10 grid place-content-center">
+          <p className="bg-gradient-to-br from-white/20 to white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+            {card.title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const cards = [
+  {
+    url: "https://plus.unsplash.com/premium_photo-1728923434154-e40f064b89c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8",
+    title: "Title 1",
+    id: 1,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1732692699555-2dc0e40eea18?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8",
+    title: "Title 2",
+    id: 2,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1732391604091-f565a77e408d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D",
+    title: "Title 3",
+    id: 3,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1732281152111-ab0ff0f9662f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D",
+    title: "Title 4",
+    id: 4,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1732823282166-2157bce662da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D",
+    title: "Title 5",
+    id: 5,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1733597572690-0510c968ca20?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D",
+    title: "Title 6",
+    id: 6,
+  },
+  {
+    url: "https://plus.unsplash.com/premium_photo-1733864822174-c469c50177a8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Title 7",
+    id: 7,
+  },
+];
