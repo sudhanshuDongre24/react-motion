32734c0145c5fe4ef5610293d976b37ca17031b4
Sudhanshu <sudhanshuDongre24@gmail.com>
2024-12-19 10:09:50 +0530

    add kaban board

--- src/examples/CustomKanban.tsx
+++ src/examples/CustomKanban.tsx
@@ -1,8 +1,11 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
+import { FaFire } from "react-icons/fa";
+import { FiPlus, FiTrash } from "react-icons/fi";
+import { motion } from "motion/react";
 
 const CustomKanban = () => {
   return (
-    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+    <div className="h-screen  w-full bg-neutral-900 text-neutral-50">
       <Board />
     </div>
   );
@@ -14,11 +17,11 @@ const Board = () => {
   const [cards, setCards] = useState(DEFAULT_CARDS);
 
   return (
-    <div className="flex h-screen w-full gap-3 overflow-scroll p-12">
+    <div className="flex h-full w-full gap-3 overflow-scroll p-12">
       <Column
         title="Backlog"
         column="backlog"
-        headingColor="text-netural-500"
+        headingColor="text-neutral-500"
         cards={cards}
         setCards={setCards}
       />
@@ -39,10 +42,11 @@ const Board = () => {
       <Column
         title="Complete"
         column="done"
-        headingColor="text-emerald-200"
+        headingColor="text-emerald-500"
         cards={cards}
         setCards={setCards}
       />
+      <BurnBarrel setCards={setCards} />
     </div>
   );
 };
@@ -64,6 +68,107 @@ const Column = ({
 }: ColumnProps) => {
   const [active, setActive] = useState(false);
 
+  const handleDragStart = (e: React.DragEvent, card: CardType) => {
+    e.dataTransfer.setData("cardId", card.id);
+  };
+
+  const handleDragEnd = (e: React.DragEvent) => {
+    const cardId = e.dataTransfer.getData("cardId");
+
+    setActive(false);
+    clearHighlight();
+
+    const indicators = getIndicators();
+    const { element } = getNearestIndicator(e, indicators);
+
+    const before = element.dataset.before || -"1";
+
+    if (before !== cardId) {
+      let copy = [...cards];
+
+      let cardToTransfer = copy.find((c) => c.id === cardId);
+      if (!cardToTransfer) return;
+
+      cardToTransfer = { ...cardToTransfer, column };
+
+      copy = copy.filter((c) => c.id !== cardId);
+
+      const moveToBack = before === "-1";
+
+      if (moveToBack) {
+        copy.push(cardToTransfer);
+      } else {
+        const insetAtIndex = copy.findIndex((el) => el.id === before);
+        copy.splice(insetAtIndex, 0, cardToTransfer);
+      }
+      setCards(copy);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    highlightIndicator(e);
+
+    setActive(true);
+  };
+
+  const clearHighlight = (els?: HTMLElement[]) => {
+    const indicators = els || getIndicators();
+
+    indicators.forEach((i) => {
+      i.style.opacity = "0";
+    });
+  };
+
+  const highlightIndicator = (e: React.DragEvent) => {
+    const indicators = getIndicators();
+
+    clearHighlight(indicators);
+
+    const el = getNearestIndicator(e, indicators);
+    el.element.style.opacity = "1";
+  };
+
+  const getNearestIndicator = (
+    e: React.DragEvent,
+    indicators: HTMLElement[]
+  ) => {
+    const DISTANCE_OFFSET = 50;
+
+    const el = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1],
+      }
+    );
+
+    return el;
+  };
+
+  const getIndicators = () => {
+    return Array.from(
+      document.querySelectorAll(
+        `[data-column="${column}"]`
+      ) as unknown as HTMLElement[]
+    );
+  };
+
+  const handleDragLeave = () => {
+    clearHighlight();
+    setActive(false);
+  };
+
   const filteredCards = cards.filter((c) => c.column === column);
 
   return (
@@ -71,17 +176,160 @@ const Column = ({
       <div className="mb-3 flex items-center justify-between">
         <h3 className={`font-medium ${headingColor}`}>{title}</h3>
         <span className="rounded text-sm text-neutral-400">
-          {filteredCards.length}
+          {filteredCards?.length}
         </span>
       </div>
       <div
+        onDrop={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
         className={`h-full w-full transition-colors ${
-          active ? "bg-neutral-800/5" : "bg-neutral-800/0"
-        } `}
+          active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
       >
-        card
+        {filteredCards?.map((c) => {
+          return <Card key={c.id} {...c} handleDragStart={handleDragStart} />;
+        })}
+        <DropIndicator beforeId={null} column={column} />
+        <AddCard column={column} setCards={setCards} />
       </div>
     </div>
+  );
+};
+
+type CardProps = CardType & {
+  handleDragStart: Function;
+};
+
+const Card = ({ title, id, column, handleDragStart }: CardProps) => {
+  return (
+    <>
+      <DropIndicator beforeId={id} column={column} />
+      <motion.div
+        layout
+        layoutId={id}
+        draggable="true"
+        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        className="cursor-grab rounded border border-neutral-700 p-3 bg-neutral-800 active:cursor-grabbing"
+      >
+        <p className="text-sm text-neutral-100">{title}</p>
+      </motion.div>
+    </>
+  );
+};
+
+const BurnBarrel = ({
+  setCards,
+}: {
+  setCards: Dispatch<SetStateAction<CardType[]>>;
+}) => {
+  const [active, setActive] = useState(false);
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setActive(true);
+  };
+  const handleDragLeave = () => {
+    setActive(false);
+  };
+  const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    const cardId = e.dataTransfer?.getData("cardId");
+
+    setCards((pv) => pv.filter((c) => c.id !== cardId));
+    setActive(false);
+  };
+
+  return (
+    <div
+      onDrop={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
+        active
+          ? "border-red-800 bg-red-800/20 text-red-500"
+          : "border-neutral-500 bg-neutral-500/20 text-neutral-500"
+      }`}
+    >
+      {active ? <FaFire className="animate-bounce" /> : <FiTrash />}
+    </div>
+  );
+};
+
+type DropIndicatorProps = {
+  beforeId: string | null;
+  column: string;
+};
+const DropIndicator = ({ beforeId, column }: DropIndicatorProps) => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    />
+  );
+};
+
+type AddCardProps = {
+  column: ColumnType;
+  setCards: Dispatch<SetStateAction<CardType[]>>;
+};
+
+const AddCard = ({ column, setCards }: AddCardProps) => {
+  const [text, setText] = useState("");
+  const [adding, setAdding] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!text.trim().length) return;
+
+    const newCard = {
+      column,
+      title: text.trim(),
+      id: Math.random().toString(),
+    };
+
+    setCards((pv) => [...pv, newCard]);
+    setAdding(false);
+  };
+
+  return (
+    <>
+      {adding ? (
+        <motion.form layout onSubmit={handleSubmit}>
+          <textarea
+            onChange={(e) => setText(e.target.value)}
+            autoFocus
+            placeholder="Add new Task..."
+            className="w-full rounded border border-violet-400 bg-violet-400/20 p-3 text-sm text-neutral-50 placeholder-violet-300 focus:outline-0"
+          />
+          <div className="mt-1.5 flex items-center justify-end gap-1.5">
+            <button
+              className="px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
+              onClick={() => setAdding(false)}
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="flex items-center gap-1.5 rounded bg-neutral-50 px-3 py-1.5 text-xs text-neutral-950 transition-colors hover:bg-neutral-300"
+            >
+              <span>Add</span>
+              <FiPlus />
+            </button>
+          </div>
+        </motion.form>
+      ) : (
+        <motion.button
+          layout
+          onClick={() => setAdding(true)}
+          className="flex w-full items-center gap-1.5 px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
+        >
+          <span>Add Card</span>
+          <FiPlus />
+        </motion.button>
+      )}
+    </>
   );
 };
 
@@ -89,9 +337,7 @@ type ColumnType = "backlog" | "todo" | "doing" | "done";
 
 type CardType = {
   title: string;
-
   id: string;
-
   column: ColumnType;
 };
 
